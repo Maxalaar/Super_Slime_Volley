6c75039ec266eb3806cd93fba7060ce22b03e865
naton <nathan.chevenard@gmail.com>
2025-12-09 13:22:25 +0100

    ADD: AI repeat action slider, christmas songs

---
+++ Scripts/UI/repeat_action_slider.gd
@@ -0,0 +1,16 @@
+extends Slider
+class_name RepeatActionSlider
+
+@export var sync : Node
+
+
+func _ready() -> void:
+	set_repeat_action_value(value)
+
+
+func _value_changed(new_value: float) -> void:
+	set_repeat_action_value(new_value)
+
+
+func set_repeat_action_value(new_value : float):
+	sync.action_repeat = roundi(max_value - new_value + min_value)
