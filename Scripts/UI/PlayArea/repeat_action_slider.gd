4e0bcc20aa6043569c60ed90fdd428af75fb0143
naton <nathan.chevenard@gmail.com>
2026-01-06 13:31:49 +0100

    MOD: Physics interpolation enabled

--- Scripts/UI/PlayArea/repeat_action_slider.gd
+++ Scripts/UI/PlayArea/repeat_action_slider.gd
@@ -4,10 +4,6 @@ class_name RepeatActionSlider
 @export var sync : Sync
 
 
-func _init() -> void:
-	SignalManager.time_scale_update.connect(_on_time_scale_updated)
-
-
 func _ready() -> void:
 	set_repeat_action_value(value)
 
@@ -17,9 +13,4 @@ func _value_changed(new_value: float) -> void:
 
 
 func set_repeat_action_value(new_value : float):
-	sync.action_repeat = roundi((max_value - new_value + min_value) / Engine.time_scale)
-	print(sync.action_repeat)
-
-
-func _on_time_scale_updated():
-	set_repeat_action_value(value)
+	sync.action_repeat = roundi(max_value - new_value + min_value)
--- Scripts/UI/PlayArea/time_scale_slider.gd
+++ Scripts/UI/PlayArea/time_scale_slider.gd
@@ -6,5 +6,6 @@ var time_scale : float = 1.0
 
 func _value_changed(new_value: float) -> void:
 	Engine.time_scale = time_scale
+	Engine.physics_ticks_per_second = roundi(60 * time_scale)
 	time_scale = new_value
 	SignalManager.emit_time_scale_update()
